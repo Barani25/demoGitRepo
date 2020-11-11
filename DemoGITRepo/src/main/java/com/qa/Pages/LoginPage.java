be44fe7069e2e5a46a0025a39acd8d8a50a706ca
Barani Ramachandran <73708082+Barani25@users.noreply.github.com>
2020-11-11 12:14:21 -0800

    Add files via upload

---
+++ DemoGITRepo/src/main/java/com/qa/Pages/LoginPage.java
@@ -0,0 +1,15 @@
+package com.qa.Pages;
+
+public class LoginPage {
+	
+	public void loginPage()
+	{
+	System.out.println("LoginPage");
+	}
+	
+	public void loginsetPage()
+	{
+	System.out.println("LoginsetPage");
+	}
+
+}
